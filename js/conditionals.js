939a3ebf56af19c72b97d4604b04c22d8e779a52
timothy <timothywsingletary@gmail.com>
2023-06-01 11:51:32 -0500

    first conditionals

---
+++ js/conditionals.js
@@ -0,0 +1,276 @@
+// "use strict";
+//
+// /* ########################################################################## */
+//
+// /**
+//  * TODO:
+//  * Create a function named `analyzeColor` that accepts a string that is a color
+//  * name as input. This function should return a message which relates to the
+//  * color stated in the argument of the function. For colors you do not have
+//  * responses written for, return a string stating so
+//  *
+//  * Example:
+//  *  > analyzeColor('blue') // returns "blue is the color of the sky"
+//  *  > analyzeColor('red') // returns "Strawberries are red"
+//  *
+//  *
+//  *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+//  *
+//  * You should use an if-else-if-else block to return different messages.
+//  *
+//  * Test your function by passing various string literals to it and
+//  * console.logging the function's return value
+//  */
+
+//  colorName = prompt('whats your favorite color')
+//
+// function analyzeColor(colorName){
+//     if (colorName === 'blue') {
+//         console.log('blue is the color of my car')
+//     } else if (colorName === 'red') {
+//         console.log('red is an obnoxious color')
+//     } else if (colorName === 'green') {
+//         console.log('money money money is green')
+//     } else if (colorName === 'orange') {
+//         console.log('an orange is orange')
+//     } else {
+//     console.log('I dont know anything about ' + colorName)
+//         }
+// }
+//
+// analyzeColor(colorName)
+
+
+
+
+
+//
+// // Don't change the next two lines!
+// // These lines create two variables for you:
+// // - `colors`: a list of the colors of the rainbow
+// // - `randomColor`: contains a single random color value from the list (this
+// //                  will contain a different color every time the page loads)
+//  var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+//  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+// /**
+//  * TODO:
+//  * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+//  * You should see a different message every time you refresh the page
+//  */
+
+//analyzeColor(randomColor)
+
+
+
+
+
+
+//
+// /**
+//  * TODO:
+//  * Comment out the code above, and refactor your function to use a switch-case statement
+//  */
+
+// switch (colorName){
+//     case 'blue':
+//         alert('blue is the color of my car')
+//         break
+//     case 'red':
+//         alert('red is an obnoxious color')
+//         break
+//     case 'green':
+//         alert('money money money is green')
+//         break
+//     case 'orange':
+//         alert('an orange is orange')
+//         break
+//     default:
+//         alert('I dont know anything about ' + colorName)
+// }
+
+
+
+
+
+//
+// /**
+//  * TODO:
+//  * Prompt the user for a color when the page loads, and pass the input from the
+//  * user to your `analyzeColor` function. Alert the return value from your
+//  * function to show it to the user.
+//  */
+
+// colorName = prompt('whats your favorite color')
+//
+// function analyzeColor(colorName){
+//     if (colorName === 'blue') {
+//         console.log('blue is the color of my car')
+//     } else if (colorName === 'red') {
+//         console.log('red is an obnoxious color')
+//     } else if (colorName === 'green') {
+//         console.log('money money money is green')
+//     } else if (colorName === 'orange') {
+//         console.log('an orange is orange')
+//     } else {
+//         console.log('I dont know anything about ' + colorName)
+//     }
+// }
+//
+// analyzeColor(colorName)
+
+
+
+
+
+
+
+
+//
+// /* ########################################################################## */
+//
+// /**
+//  * TODO:
+//  * Suppose there's a promotion in Walmart, each customer is given a randomly
+//  * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+//  * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+//  * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+//  * everything for free!.
+//  *
+//  * Write a function named `calculateTotal` which accepts a lucky number and total
+//  * amount, and returns the discounted price.
+//  *
+//  * Example:
+//  * calculateTotal(0, 100) // returns 100
+//  * calculateTotal(4, 100) // returns 50
+//  * calculateTotal(5, 100) // returns 0
+//  *
+//  * Test your function by passing it various values and checking for the expected
+//  * return value.
+//  */
+//
+
+// let luckyNumber = Math.floor((Math.random() * 6));
+//  alert('Your lucky number is ' + luckyNumber  + '!')
+// let cartTotal = prompt('whats your cart total?')
+//
+// function calculateTotal(luckyNumber, cartTotal){
+//     if (luckyNumber === 0){
+//         alert('your cart total is $' + cartTotal)
+//     }else if (luckyNumber === 1){
+//         alert('your cart total is $' + (.9 * (cartTotal * 1)))
+//     }else if (luckyNumber === 2){
+//         alert('your cart total is $' + (.75 * (cartTotal * 1)))
+//     }else if (luckyNumber === 3){
+//         alert('your cart total is $' + (.65 * (cartTotal * 1)))
+//     }else if (luckyNumber === 4){
+//         alert('your cart total is $' + (.5 * (cartTotal * 1)))
+//     }else if (luckyNumber === 5){
+//         alert('Its on the house!')
+//     }
+// }
+//
+// calculateTotal(luckyNumber, cartTotal)
+
+
+
+
+
+
+
+// /**
+//  * TODO:
+//  * Uncomment the line below to generate a random number between 0 and 5.
+//  * (In this line of code, 0 is inclusive, and 6 is exclusive)
+//  * Prompt the user for their total bill, then use your `calculateTotal` function
+//  * and alerts to display to the user what their lucky number was, what their
+//  * price before the discount was, and what their price after the discount is.
+//  */
+// // Generate a random number between 0 and 6
+// // var luckyNumber = Math.floor(Math.random() * 6);
+
+// let luckyNumber = Math.floor((Math.random() * 6));
+// alert('Your lucky number is ' + luckyNumber  + '!')
+// let cartTotal = prompt('whats your cart total?')
+//
+// function calculateTotal(luckyNumber, cartTotal){
+//     if (luckyNumber === 0){
+//         alert('your cart total is $' + cartTotal)
+//     }else if (luckyNumber === 1){
+//         alert('your cart total is $' + (.9 * (cartTotal * 1)))
+//     }else if (luckyNumber === 2){
+//         alert('your cart total is $' + (.75 * (cartTotal * 1)))
+//     }else if (luckyNumber === 3){
+//         alert('your cart total is $' + (.65 * (cartTotal * 1)))
+//     }else if (luckyNumber === 4){
+//         alert('your cart total is $' + (.5 * (cartTotal * 1)))
+//     }else if (luckyNumber === 5){
+//         alert('Its on the house!')
+//     }
+// }
+//
+// calculateTotal(luckyNumber, cartTotal)
+
+
+
+
+
+
+
+
+//
+// /**
+//  * TODO:
+//  * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+//  * would like to enter a number. If they click 'Ok', prompt the user for a
+//  * number, then use 3 separate alerts to tell the user:
+//  *
+//  * - whether the number is even or odd
+//  * - what the number plus 100 is
+//  * - if the number is negative or positive
+//  *
+//  * Do *NOT* display any of the above information
+//  * if the user enters a value that is not of the number data type.
+//  * Instead, use an alert to inform them of the incorrect input data type.
+//  *
+//  *
+//  * Can you refactor your code to use functions?
+//  * HINT: The way we prompt for a value could be improved
+//  */
+
+let userNumber;
+
+let wouldLikeToEnterNumber = confirm('Would you like to enter a number')
+
+if (wouldLikeToEnterNumber === true) {
+    userNumber = prompt('enter a number')
+}else {
+    alert('Thats okay see ya later')
+}
+
+let userNumberFixed = userNumber * 1
+
+let isEven = userNumberFixed % 2 == 0
+let isPositive = userNumberFixed > 0
+
+function numberFacts(userNumberFixed){
+    if (isNaN(userNumberFixed)){
+        alert('That data type is incorrect \n Try again')
+    }else if ( isEven && isPositive){
+        alert('This number is even')
+        alert(userNumberFixed + 100)
+        alert('This number is positive')
+    }else if ( !isEven && isPositive){
+        alert('This number is odd')
+        alert(userNumberFixed + 100)
+        alert('This number is positive')
+    }else if ( !isEven && !isPositive){
+        alert('This number is odd')
+        alert(userNumberFixed + 100)
+        alert('This number is negative')
+    }else if ( isEven && !isPositive){
+        alert('This number is even')
+        alert(userNumberFixed + 100)
+        alert('This number is negative')
+    }
+}
+numberFacts(userNumberFixed)
